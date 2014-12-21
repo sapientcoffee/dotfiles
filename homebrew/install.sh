d5cbdd9b479377bf1787151747b50cd96ddef81b
Rob Edwards <rob.j.edwards@gmail.com>
2014-12-21 22:49:19 +0000

    mostly grc updates, some other tidy ups

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,5 +12,6 @@ if test ! $(which brew)
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
 # Install homebrew packages
-	brew install grc coreutils spark
+	#brew install grc coreutils spark
+	brew install grc coreutils spark z ack git
 	exit 0

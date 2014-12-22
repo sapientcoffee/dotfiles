670d8d33f1193bb8a10fa2e941c4e286001f7087
Rob Edwards <rob.j.edwards@gmail.com>
2014-12-22 17:58:41 +0000

    commented the press-and-hold option

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -7,7 +7,7 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+#defaults write -g ApplePressAndHoldEnabled -bool false
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 # Always open everything in Finder's list view. This is important.

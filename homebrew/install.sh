4425d5801655fd0ffbcd0cf0e163396ff291c4b9
Rob Edwards <rob.j.edwards@gmail.com>
2016-06-03 21:47:18 +0100

    check to see if running on a Mac before installing

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,16 +1,17 @@
-
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
-# Check for Homebrew
-if test ! $(which brew)
-	then
-	echo " Installing Homebrew for you."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-# Install homebrew packages
-	brew install grc coreutils spark z ack git autoconf git-extras htop-osx python python3 openconnect openssl wget tree tmux
-	exit 0
+# Scirpt to install and setup Homebrew if executed on a Mac
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    # Check for Homebrew already installed
+    if test ! $(which brew)
+        then
+            echo " Installing Homebrew for you."
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+    # Install homebrew packages
+    brew install grc coreutils spark z ack git autoconf git-extras htop-osx python python3 openconnect openssl wget tree tmux
+    exit 0
+else
+    echo "Not running OSX therefore skipping Homebrew install"
+    exit 0
+fi

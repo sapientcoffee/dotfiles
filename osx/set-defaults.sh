c9847cc785ba634bea967bd1149b60cda3c929ff
Rob Edwards <rob.j.edwards@gmail.com>
2017-01-12 18:11:29 +0000

    Tidy up logic

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -33,6 +33,7 @@ if [[ "$unamestr" == 'Darwin' ]]; then
     #defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
     #defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
     #defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+    com.apple.finder AppleShowAllFiles YES.
     exit
 else
     exit

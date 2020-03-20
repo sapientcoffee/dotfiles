8ab48b24489f624296f98632fd050c6a8e129a37
Rob Edwards <rob.j.edwards@gmail.com>
2020-03-20 16:47:53 +0000

    hack to get to work

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,32 +9,31 @@ set -u # fail if we hit unset variables
 main() {
 # Scirpt to install and setup Homebrew if executed on a Mac
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    # Check for Homebrew already installed
-	echo "Checking if brew is already installed .... if not will install."
-    if test ! $(which brew)
-        then
-            echo " Installing Homebrew for you."
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		brew doctor
-		brew install cask
-		brew doctor
-	# homebrew cask updater: https://github.com/buo/homebrew-cask-upgrade
-	brew tap buo/cask-upgrade
+    if [ "$(uname -s)" == "Darwin" ]; then
+        # Check for Homebrew already installed
+        echo "Checking if brew is already installed .... if not will install."
+        if test ! $(which brew)
+            then
+                echo " Installing Homebrew for you."
+                ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            brew doctor
+            brew install cask
+            brew doctor
+        # homebrew cask updater: https://github.com/buo/homebrew-cask-upgrade
+        brew tap buo/cask-upgrade
         fi
-    # Install homebrew packages
-	echo "Lets install a few packages that will be useful"
-    brew install bash-completion grc coreutils spark z ack git autoconf git-extras htop-osx python python3 openconnect openssl wget tree tmux kubespy kube-ps1 mas zsh zsh-completions
-brew cask install google-chrome
-brew cask install skype
-brew cask install slack
-brew cask install iterm2
-    echo "Done installing brew stuff"
-	# exit 0
-else
-    echo "Not running OSX therefore skipping Homebrew install"
-    # exit 0
-fi
+        # Install homebrew packages
+        echo "Lets install a few packages that will be useful"
+        brew install bash-completion grc coreutils spark z ack git autoconf git-extras htop-osx python python3 openconnect openssl wget tree tmux kubespy kube-ps1 mas zsh zsh-completions
+        brew cask install google-chrome
+        brew cask install skype
+        brew cask install slack
+        brew cask install iterm2
+        echo "Done installing brew stuff"
+    else
+        echo "Not running OSX therefore skipping Homebrew install"
+
+    fi
 }
 
 main "$@"
--- linux/install.sh
+++ linux/install.sh
@@ -4,7 +4,9 @@ if [ -f /etc/debian_version ]; then
     echo "Detected a Debian based system"
     sudo apt update
     sudo apt -y upgrade
-    sudo apt -y install docker jq  
+    sudo apt -y install docker jq grc direnv
+
+    # curl -sfL https://direnv.net/install.sh | bash
 
 else
     echo "Did not detect a know Linux OS - it might be Linux just never setup for it"

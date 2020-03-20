406ca0c3ec775f137df3ea9358f96f3cf4053fc3
clijockey <rob.j.edwards@gmail.com>
2020-03-20 14:55:27 +0000

    Linux installation packages

---
+++ linux/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f /etc/debian_version ]; then
+    echo "Detected a Debian based system"
+    sudo apt update
+    sudo apt -y upgrade
+    sudo apt -y docker jq  
+
+else
+    echo "Did not detect a know Linux OS - it might be Linux just never setup for it"
+
+fi

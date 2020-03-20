919504d4b38d634df014308cac18da5e7e637cfb
Rob Edwards <rob.j.edwards@gmail.com>
2020-03-20 17:52:39 +0000

    fix some stuff around fonts

--- linux/install.sh
+++ linux/install.sh
@@ -4,7 +4,7 @@ if [ -f /etc/debian_version ]; then
     echo "Detected a Debian based system"
     sudo apt update
     sudo apt -y upgrade
-    sudo apt -y install docker jq grc direnv
+    sudo apt -y install docker jq grc direnv python-pip python3-pip
 
     # curl -sfL https://direnv.net/install.sh | bash
 

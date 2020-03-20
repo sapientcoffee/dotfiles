554348b107a3c598f8fe6bda83712b42e18628d2
clijockey <rob.j.edwards@gmail.com>
2020-03-20 15:24:34 +0000

    tidy

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@ set -u # fail if we hit unset variables
 main() {
 # Scirpt to install and setup Homebrew if executed on a Mac
 
-if [ "$(uname -s)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
     # Check for Homebrew already installed
 	echo "Checking if brew is already installed .... if not will install."
     if test ! $(which brew)

f4b3fd5b7c084b38a9fa6fb784b92af722188ead
SapientCoffee <rob.j.edwards@gmail.com>
2025-07-25 21:42:44 -0700

    fix(nvim): Explicitly configure eslint path for null-ls

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,4 +1,18 @@
 return {
   "nvimtools/none-ls.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.eslint.with({
+          command = "/usr/local/bin/eslint",
+        }),
+        null_ls.builtins.formatting.eslint.with({
+          command = "/usr/local/bin/eslint",
+        }),
+      },
+    })
+  end,
 }

7c41135bae5e93e602df328cfd808524e7e0acbc
SapientCoffee <rob.j.edwards@gmail.com>
2025-07-25 21:19:41 -0700

    feat(nvim): Add Comment, Neogit, and indent-blankline plugins

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,9 +1,8 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required for neogit
+    "sindrets/diffview.nvim", -- optional - for diff viewing
   },
   config = true,
 }

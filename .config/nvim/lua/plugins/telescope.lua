0f334273f81a81676e69accd306fc88097542054
SapientCoffee <rob.j.edwards@gmail.com>
2025-07-25 21:30:45 -0700

    feat(nvim): Add telescope-fzf-native for performance

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,15 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      -- This is needed to ensure the fzf native extension is built
+      build = "make",
+    },
+  },
+  config = function()
+    -- Load the fzf extension
+    require("telescope").load_extension("fzf")
+  end,
 }

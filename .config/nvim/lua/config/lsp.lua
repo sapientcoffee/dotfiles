0b1dc6c0ad0b3f69d365c9f4ec4b28e4a5d7fdd5
SapientCoffee <rob.j.edwards@gmail.com>
2025-07-25 20:59:55 -0700

    fix(nvim): Correct mason-lspconfig setup

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -2,14 +2,19 @@ local lspconfig = require("lspconfig")
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
+-- Enable Mason for plugin management
 mason.setup()
 
+-- Configure mason-lspconfig to bridge Mason and lspconfig
 mason_lspconfig.setup({
+  -- A list of servers to automatically install if they're not already installed
   ensure_installed = { "lua_ls" },
-})
 
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    lspconfig[server_name].setup({})
-  end,
+  -- This is the a default handler that will be called for each installed server.
+  -- It calls the server's `setup` function with an empty configuration table.
+  handlers = {
+    function(server_name)
+      lspconfig[server_name].setup({})
+    end,
+  },
 })

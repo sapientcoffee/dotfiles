902a02a3bed621c4ac73dc6873d658d86965be5e
SapientCoffee <rob.j.edwards@gmail.com>
2025-07-25 20:57:41 -0700

    fix(nvim): Correct treesitter parser and LSP config

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,6 +3,7 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
 mason.setup()
+
 mason_lspconfig.setup({
   ensure_installed = { "lua_ls" },
 })
--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "c", "lua", "vim", "help" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc" },
   sync_install = false,
   auto_install = true,
   highlight = {
